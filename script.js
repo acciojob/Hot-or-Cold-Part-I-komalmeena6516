1ce305a1577d74633fe5ab5422f3476412af6f65
Komal Meena <80938016+komalmeena6516@users.noreply.github.com>
2023-02-11 00:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
-
+function GenerateRandom(){
+	let x=Math.floor((Math.random() * 20) + 1);
+	document.getElementById("num").innerHTML=x;
+	let generatedValue=document.getElementById("num").innerHTML;
+	let guessedValue=document.getElementById("guess").value;
+	if(Math.abs(generatedValue-guessedValue)==0)
+	{
+		document.getElementById("respond").innerHTML="Hot";
+	}
+	else{
+		document.getElementById("respond").innerHTML="Cold";
+	}
+}
